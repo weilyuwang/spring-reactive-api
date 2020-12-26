4868ef2e63d923b568fdffb59c39d5907cfb9083
weilyuwang <weilyuwang@gmail.com>
2020-12-26 00:40:14 -0500

    unit tests for reactive

--- src/main/java/com/weilyu/reactiveapi/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/main/java/com/weilyu/reactiveapi/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.weilyu.reactiveapi.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -17,5 +18,31 @@ public class FluxAndMonoTest {
                 System.err::println,
                 () -> System.out.println("Completed - This will not be showed either")
         );
+    }
+
+
+    @Test
+    public void fluxTestElementsWithoutError() {
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxTestElementsWithError() {
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                .concatWith(Flux.error(new RuntimeException("Expected Error")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+                .verifyError(RuntimeException.class);
     }
 }
