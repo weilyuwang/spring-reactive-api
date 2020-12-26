bcc2fcab3d48caa276893167f989d626432f99c2
weilyuwang <weilyuwang@gmail.com>
2020-12-26 00:03:44 -0500

    playground

--- src/main/java/com/weilyu/reactiveapi/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/main/java/com/weilyu/reactiveapi/fluxandmonoplayground/FluxAndMonoTest.java
@@ -7,8 +7,15 @@ public class FluxAndMonoTest {
 
     @Test
     public void fluxTest() {
-       Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring");
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                .concatWith(Flux.error(new RuntimeException("Expected Error")))
+                .concatWith(Flux.just("After Error - This will not be showed"))
+                .log();
 
-       stringFlux.subscribe(System.out::println);
+        stringFlux.subscribe(
+                System.out::println,
+                System.err::println,
+                () -> System.out.println("Completed - This will not be showed either")
+        );
     }
 }
