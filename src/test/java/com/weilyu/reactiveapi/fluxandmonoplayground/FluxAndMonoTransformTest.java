151f517afe5adab92926b06e57c71b39f93f592e
weilyuwang <weilyuwang@gmail.com>
2020-12-26 21:23:26 -0500

    map and filter

---
+++ src/test/java/com/weilyu/reactiveapi/fluxandmonoplayground/FluxAndMonoTransformTest.java
@@ -0,0 +1,50 @@
+package com.weilyu.reactiveapi.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FluxAndMonoTransformTest {
+    List<String> names = Arrays.asList("Adam", "Anna", "Jack", "Jerry");
+
+    @Test
+    void transformUsingMap() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .map(String::toUpperCase)
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("ADAM", "ANNA", "JACK", "JERRY")
+                .verifyComplete();
+    }
+
+
+    @Test
+    void transformUsingMapLengthRepeat() {
+        Flux<Integer> namesFlux = Flux.fromIterable(names)
+                .map(String::length)
+                .repeat(1)
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext(4, 4, 4, 5, 4, 4, 4, 5)
+                .verifyComplete();
+    }
+
+    @Test
+    void transformUsingMapFilter() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .filter(s -> s.length() > 4)
+                .map(String::toUpperCase)
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("JERRY")
+                .verifyComplete();
+    }
+
+
+}
