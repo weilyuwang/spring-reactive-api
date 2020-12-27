d7db493c60fd0a4a9743a5aac1ee84a6689e4bec
weilyuwang <weilyuwang@gmail.com>
2020-12-26 22:27:50 -0500

    combine two flux

---
+++ src/test/java/com/weilyu/reactiveapi/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -0,0 +1,82 @@
+package com.weilyu.reactiveapi.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FluxAndMonoCombineTest {
+
+    @Test
+    void combineUsingMerge() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.merge(flux1, flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+
+    @Test
+    void combineUsingMergeWithDelay() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+
+        // flux2 will not wait until flux1 is completed
+        // Sequence will be A->D ... B->E ... C->F
+        Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
+
+        Flux<String> mergedFlux = Flux.merge(flux1, flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                //  .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+
+    @Test
+    void combineUsingConcat() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.concat(flux1, flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    void combineUsingConcatWithDelay() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
+
+        // With concat(), flux2 will wait for flux1 to complete i.e. order maintained
+        Flux<String> mergedFlux = Flux.concat(flux1, flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    void combineUsingZip() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.zip(flux1, flux2, String::concat);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("AD", "BE", "CF")
+                .verifyComplete();
+    }
+}
