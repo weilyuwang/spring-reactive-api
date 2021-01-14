3c1d0ccd68067a597303a12747771c3036ae78d2
weilyuwang <weilyuwang@gmail.com>
2021-01-13 19:09:48 -0500

    more test approaches

--- src/test/java/com/weilyu/reactiveapi/controller/v1/ItemControllerTest.java
+++ src/test/java/com/weilyu/reactiveapi/controller/v1/ItemControllerTest.java
@@ -12,12 +12,16 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
-@DirtiesContext  // It indicates the associated test or class modifies the ApplicationContext. It tells the testing framework to close and recreate the context for later tests.
+@DirtiesContext
+// It indicates the associated test or class modifies the ApplicationContext. It tells the testing framework to close and recreate the context for later tests.
 @AutoConfigureWebTestClient   // Annotation that can be applied to a test class to enable a WebTestClient.
 @ActiveProfiles("test")
 class ItemControllerTest {
@@ -46,12 +50,41 @@ class ItemControllerTest {
     }
 
     @Test
-    public void getAllItems() {
+    void getAllItems() {
         webTestClient.get().uri("/api/v1/items")
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBodyList(Item.class)
                 .hasSize(4);
+    }
+
+    @Test
+    void getAllItems_approach2() {
+        webTestClient.get().uri("/api/v1/items")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class) // List<Item>
+                .hasSize(4)
+                .consumeWith(response -> {
+                    List<Item> items = response.getResponseBody();
+                    assert items != null;
+                    items.forEach(item -> assertNotNull(item.getId()));
+                });
+    }
+
+    @Test
+    void getAllItems_approach3() {
+        Flux<Item> itemsFlux = webTestClient.get().uri("/api/v1/items")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(Item.class) // Returns a Flux<Item>
+                .getResponseBody();
+
+        StepVerifier.create(itemsFlux.log())
+                .expectNextCount(4)
+                .verifyComplete();
     }
 }
